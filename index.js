44dbf3053fcf840c853e437a4b25cefc5f9fae92
Alexandre Jezus <alexandrejezus.p@gmail.com>
2024-06-24 11:30:04 -0300

    feat init

---
+++ index.js
@@ -0,0 +1,14 @@
+let entrada = [];
+entrada = entrada.split("");
+let anagrama = [];
+
+console.log("Digite uma palavra para fazer o anagrama.");
+
+process.stdin.on("data", function (data) {
+    entrada = data, toString().trim());
+    for (let i = 0; i < entrada.length; i++) {
+        const element = entrada[i];
+        
+    }
+
+    console.log(entrada.join(""));
