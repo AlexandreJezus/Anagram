e80834b0e45808b5c3f413cfac0b53ce42c6ace8
Alexandre Jezus <alexandrejezus.p@gmail.com>
2024-06-25 08:32:14 -0300

    anagramas

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-let entrada = [];
-entrada = entrada.split("");
-let anagrama = [];
+let palavra;
 
 console.log("Digite uma palavra para fazer o anagrama.");
 
 process.stdin.on("data", function (data) {
-    entrada = data, toString().trim());
-    for (let i = 0; i < entrada.length; i++) {
-        const element = entrada[i];
-        
-    }
+  (palavra = data), toString().trim();
 
-    console.log(entrada.join(""));
+  palavra = palavra.split("");
+
+  for (let i = 0; i < palavra.length; i++) {
+    for (let j = 0; j < palavra.length; j++) {
+      let aux = palavra[i];
+      palavra[i] = palavra[j];
+      palavra[j] = aux;
+
+      anagramas.push(palavra.join(""));
+
+      if (!anagramas.includes(palavra.join("")))
+        anagramas.push(palavra.join(""));
+      console.log(palavra.join(""));
+    }
+  }
+});
