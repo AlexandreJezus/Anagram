6332cd1f8cdd795195c877cfdac352bf42955838
Alexandre Jezus <alexandrejezus.p@gmail.com>
2024-06-27 08:25:38 -0300

    anagrama

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-
-=======
 let entrada = "";
 entrada = entrada.split("");
 let anagrama = "";
 
-
 console.log("Digite uma palavra para fazer o anagrama.");
 
-process.stdin.on("data", function (data) { 
-                 
+process.stdin.on("data", function (data) {
   (palavra = data), toString().trim();
 
-  palavra = palavra.split("");
+  palavra = palavra.splice("");
 
   for (let i = 0; i < palavra.length; i++) {
     for (let j = 0; j < palavra.length; j++) {
